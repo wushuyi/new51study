41e07e34bd9a7651b18e00e4c7e9499631e769be
wushuyi <wushuyi541@gmail.com>
2017-12-29 17:24:50 +0800

    work-bg-cover

---
+++ components/contests/ui/work-bg-cover/index.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import Style from './style.scss'
+import { consts } from 'config/settings'
+import defaultBgCover from '/static/images/bg/bg_no_pic_default.jpg'
+
+const BgCoverQuery = `?imageView2/1/w/${191 * 2}/h/${115 * 2}/100`
+const vedioBgCoverQuery = consts.vedioSufFixStr + `/w/${191 * 2}/h/${115 * 2}`
+
+export default class WorkBgCover extends React.PureComponent {
+  static propTypes = {
+    bgCover: PropTypes.any,
+    isVideoType: PropTypes.any
+  }
+
+  static defaultProps = {
+    bgCover: defaultBgCover,
+    isVideoType: true
+  }
+
+  render() {
+    const {isVideoType, bgCover} = this.props
+    const style = {
+      backgroundImage: `url('${isVideoType ? bgCover + vedioBgCoverQuery : bgCover + BgCoverQuery}')`
+    }
+    return (
+      <Fragment>
+        <div className='bg-cover' style={style}>
+          {isVideoType && <div className="bg-cover-icon"/>}
+        </div>
+        {/*language=CSS*/}
+        <style jsx>{Style}</style>
+      </Fragment>
+    )
+  }
+}
--- components/contests/ui/work-item/index.js
+++ components/contests/ui/work-item/index.js
@@ -1,46 +1,22 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Style from './style.scss'
-import Style2 from './bg-cover.scss'
-import { px2rem } from 'utils/hotcss'
-import defaultBgCover from '/static/images/bg/bg_no_pic_default.jpg'
 import Avatar from 'components/ui/avatar'
-import { consts } from 'config/settings'
+import WorkBgCover from 'components/contests/ui/work-bg-cover'
+
 
 const defbgCover = 'http://7xpx8n.com1.z0.glb.clouddn.com/media_blog_75809900_2017_02_20_08_57_27_365_4980_wh2068x2668.jpg'
 const defvbgCover = 'http://7xpx8n.com1.z0.glb.clouddn.com/media_blog_76247990_2017_12_27_13_36_55_781_5698_wh1280x720.mp4'
-const BgCoverQuery = `?imageView2/1/w/${191 * 2}/h/${115 * 2}/100`
-const vedioBgCoverQuery = consts.vedioSufFixStr + `/w/${191 * 2}/h/${115 * 2}`
-
-class BgCover extends React.PureComponent {
-  static propTypes = {
-    bgCover: PropTypes.any,
-    isVideoType: PropTypes.any
-  }
-
-  render() {
-    const {isVideoType, bgCover} = this.props
-    const style = {
-      backgroundImage: `url('${isVideoType ? bgCover + vedioBgCoverQuery : bgCover + BgCoverQuery}')`
-    }
-    return (
-      <Fragment>
-        {
-          isVideoType ? (
-            <div className='bgcover' style={style}>
-              <div className="bgcover-icon"/>
-            </div>
-          ) : (
-            <div className='bgcover' style={style}/>
-          )
-        }
-        <style jsx>{Style2}</style>
-      </Fragment>
-    )
-  }
-}
 
 export default class WorkItem extends React.PureComponent {
+
+  static propTypes = {
+    AvatarProps: PropTypes.any,
+    bgCover: PropTypes.any,
+    discussCount: PropTypes.any,
+    isVideoType: PropTypes.any,
+    praiseCount: PropTypes.any
+  }
 
   static defaultProps = {
     bgCover: defvbgCover || defbgCover,
@@ -63,7 +39,7 @@ export default class WorkItem extends React.PureComponent {
     return (
       <Fragment>
         <div className='ui-work-tiem-warp'>
-          <BgCover {...bgCoverProps}/>
+          <WorkBgCover {...bgCoverProps}/>
           <div className="ui-banner-warp">
             <div className="avatar-item">
               <Avatar size={22} {...AvatarProps}/>
