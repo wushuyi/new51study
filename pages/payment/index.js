c0518a4ecfed91cf04592ea6055694dad978fe42
wushuyi <wushuyi541@gmail.com>
2018-03-22 18:03:33 +0800

    订单支付 api

--- pages/payment/index.js
+++ pages/payment/index.js
@@ -13,9 +13,10 @@ import Modal from 'antd-mobile/lib/modal'
 
 import TitleItem from 'components/sign-up/ui/title-item'
 import InfoList from 'components/payment/info-list'
-import GoPay from 'components/payment/goPay'
 import BottomOperation from 'components/payment/bottom-operation'
 import { Price, ChooseList } from 'components/payment/goPay'
+import { is_weixin } from 'utils/runEnv'
+import Router from 'next/router'
 
 const {alert} = Modal
 
@@ -44,7 +45,7 @@ class Page extends React.Component {
     authData && store.dispatch(actions.syncAuthData(authData))
     try {
       const def = deferred()
-      store.dispatch(actions.initPage(query.orderNo, def, token))
+      store.dispatch(actions.initPage('TTBSBM0CRUZK88909', def, token))
       await def.promise
     } catch (err) {
       return {
@@ -70,6 +71,81 @@ class Page extends React.Component {
     this.setState({
       isMount: true
     })
+
+    const {actions} = this.props
+
+    //微信返回支付
+    const {code, state, orderNo} = Router.router.query
+    if (code && state && orderNo) {
+      const def = deferred()
+      actions.getWxAppid(code, def)
+      def.promise.then(
+        (payData) => {
+          this.wx_subscription_pay(payData)
+        },
+      )
+
+    }
+  }
+
+  saveChooseList = (n) => {
+    this.refChooseList = n
+  }
+
+  onSubmit = () => {
+    const {actions} = this.props
+    const payType = this.refChooseList.state.payType
+    if (!payType) {
+      alert('请选择支付方式')
+      return false
+    }
+    if (payType === 'wxpay') {
+      const def = deferred()
+      if (is_weixin()) {
+        actions.goWxAuthorize(def)
+      } else {
+        actions.goWxPay(def)
+      }
+      def.promise.then(
+        url => {
+          window.location.href = url
+          // console.log(url)
+        },
+      )
+    } else if (payType === 'alipay') {
+      const def = deferred()
+      actions.goAlipay(def)
+      def.promise.then(
+        url => {
+          window.location.href = url
+          // console.log(url)
+        },
+      )
+    }
+  }
+
+  wx_subscription_pay (data = {}) {
+    WeixinJSBridge && WeixinJSBridge.invoke(
+      'getBrandWCPayRequest',
+      data,
+      function (res) {
+        let msg = res.err_msg
+        if (msg === 'get_brand_wcpay_request:ok') {
+          alert('支付成功')
+          // browserHistory.push(`/goods/orderdetail/${commodityId}/${orderNo}?payType=WXPAY`)
+        } else {
+          let err_msg
+          if (msg === 'get_brand_wcpay_request:cancel') {
+            err_msg = '您取消了微信支付'
+          } else if (msg === 'get_brand_wcpay_request:fail') {
+            err_msg = `微信支付失败:${res.err_desc},错误码:${res.err_code}`
+          } else {
+            err_msg = `${msg},desc:${res.err_desc}`
+          }
+          alert(err_msg)
+        }
+      }
+    )
   }
 
   render () {
@@ -92,9 +168,9 @@ class Page extends React.Component {
         <Share/>
         <TitleItem title="支付订单"/>
         {payInfoProps && <InfoList sourceData={payInfoProps}/>}
-        <ChooseList/>
+        <ChooseList ref={this.saveChooseList}/>
         {totalProps && <Price money={totalProps}/>}
-        <BottomOperation/>
+        <BottomOperation onClick={this.onSubmit}/>
       </Layout>
     )
   }
@@ -108,6 +184,10 @@ export default withRedux(Page, function (KeaContext) {
       mainLogic, [
         'initPage',
         'syncAuthData',
+        'goAlipay',
+        'goWxPay',
+        'goWxAuthorize',
+        'getWxAppid',
       ]
 
     ],
--- utils/runEnv.js
+++ utils/runEnv.js
@@ -1,2 +1,34 @@
 export const isBrowser = typeof window !== 'undefined';
 export const isServer = !isBrowser;
+
+/***
+ * 是否是微信客户端
+ */
+
+export function is_weixin() {
+  let isWeiXin = false;
+  let ua = window.navigator.userAgent.toLowerCase();
+  if (ua.match(/MicroMessenger/i) === 'micromessenger') {
+    isWeiXin = true;
+  }
+  return isWeiXin;
+};
+
+/***
+ * 是否是QQ客户端
+ */
+export function is_QQ() {
+  let isQQ = false;
+  let ua = window.navigator.userAgent.toLowerCase();
+  if (ua.match(/qq/i) === 'qq' && ua.match(/Pixel/i) === 'pixel') {
+    isQQ = true;
+  }
+  return isQQ;
+}
+
+/***
+ * 是否是google
+ */
+export function isChrome() {
+  return window.google && window.chrome;
+}
